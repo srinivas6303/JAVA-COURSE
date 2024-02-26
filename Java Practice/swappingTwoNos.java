44c758c06eb0369231f89fc95e7efe7c6ade9b91
D.srinivas <126507402+srinivas6303@users.noreply.github.com>
2024-02-26 15:13:19 +0000

    adding another logic to swapping two no

--- Java Practice/swappingTwoNos.java
+++ Java Practice/swappingTwoNos.java
@@ -10,13 +10,25 @@ public class swappingTwoNos {
         // System.out.println("Before swapping:"+a+ " "+b);
 
         //without using temp variable
-        int a=1;
-        int b=2;
+        // int a=1;
+        // int b=2;
+        // System.out.println("Before swapping:"+a+ " "+b);
+        // a=a+b;
+        // b=a-b;
+        // a=a-b;
+        // System.out.println("After swapping:"+a+ " "+b);
+
+
+        //using Bitwise Operator
+        int a=5;
+        int b=10;
         System.out.println("Before swapping:"+a+ " "+b);
-        a=a+b;
-        b=a-b;
-        a=a-b;
-        System.out.println("Before swapping:"+a+ " "+b);
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("After swapping:"+a+ " "+b);
+
+
 
 
     }
