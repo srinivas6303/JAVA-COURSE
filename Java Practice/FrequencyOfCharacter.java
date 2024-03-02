c8174c673fd6f393c3931ebe289ade65db4236fc
D.srinivas <126507402+srinivas6303@users.noreply.github.com>
2024-03-02 11:12:27 +0000

    added encapsulation.java and FrequencyOfCharacter.java

---
+++ Java Practice/FrequencyOfCharacter.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class FrequencyOfCharacter {
+    public static void main(String[] args) {
+        String str = "Srinivas";
+        str = str.toUpperCase();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            if (map.get(ch) == null) {
+                map.put(ch, 1);
+            } else {
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+
+        System.out.println("Occurrences of each character in the string: ");
+        for (Character c : map.keySet()) {
+            System.out.println(c + " : " + map.get(c));
+        }
+    }
+}
